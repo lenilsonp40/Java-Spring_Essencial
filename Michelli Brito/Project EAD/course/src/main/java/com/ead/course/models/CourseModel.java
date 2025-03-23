bbdff6841e121d47817698efdae9120156b2f32f
Lenilson Soares <lenilsonp40@gmail.com>
2025-03-23 19:14:15 -0300

    lesson

--- Michelli Brito/Project EAD/course/src/main/java/com/ead/course/models/CourseModel.java
+++ Michelli Brito/Project EAD/course/src/main/java/com/ead/course/models/CourseModel.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -46,7 +48,8 @@ public class CourseModel implements Serializable {
 
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @OneToMany(mappedBy = "course")
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
+    @Fetch(FetchMode.SUBSELECT)
     private Set<ModuleModel> modules;
 
 }
--- Michelli Brito/Project EAD/course/src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
+++ Michelli Brito/Project EAD/course/src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
@@ -1,13 +1,50 @@
 package com.ead.course.services.impl;
 
 import com.ead.course.Repository.CourseRepository;
+import com.ead.course.Repository.LessonRepository;
+import com.ead.course.Repository.ModuleRepository;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
 import com.ead.course.services.ModuleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ModuleServiceImpl implements ModuleService {
 
     @Autowired
-    CourseRepository courseRepository;
+    ModuleRepository moduleRepository;
+
+    @Autowired
+    LessonRepository lessonRepository;
+
+    @Transactional
+    @Override
+    public void delete(ModuleModel moduleModel) {
+        List<LessonModel> lessonModelList = lessonRepository.findAllLessonsIntoModule(moduleModel.getModuleId());
+        if (!lessonModelList.isEmpty()){
+            lessonRepository.deleteAll(lessonModelList);
+        }
+        moduleRepository.delete(moduleModel);
+    }
+
+    @Override
+    public ModuleModel save(ModuleModel moduleModel) {
+        return moduleRepository.save(moduleModel);
+    }
+
+    @Override
+    public Optional<ModuleModel> findModuleIntoCourse(UUID courseId, UUID moduleId) {
+        return moduleRepository.findModuleIntoCourse(courseId, moduleId);
+    }
+
+    @Override
+    public Optional<ModuleModel> findById(UUID moduleId) {
+        return moduleRepository.findById(moduleId);
+    }
 }
