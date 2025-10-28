f32c0478eeff01217a7ae53311a53b18e0958f2b
Lenilson Soares <lenilsonp40@gmail.com>
2025-10-27 23:21:32 -0300

    crud

--- Spring Boot Expert - Douglas sousa/produtosapi/src/main/java/io/github/lenilson/produtosapi/controller/ProdutoController.java
+++ Spring Boot Expert - Douglas sousa/produtosapi/src/main/java/io/github/lenilson/produtosapi/controller/ProdutoController.java
@@ -2,10 +2,7 @@ package io.github.lenilson.produtosapi.controller;
 
 import io.github.lenilson.produtosapi.model.Produto;
 import io.github.lenilson.produtosapi.repository.ProdutoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/produtos")
@@ -26,6 +23,28 @@ public class ProdutoController {
         produtoRepository.save(produto);
         System.out.println("Salvando produto" + produto);
         return produto;
+
+    }
+
+    @GetMapping("/{id}")
+    public Produto obterProduto(@PathVariable("id") String id){
+        System.out.println("Obtendo produto com id: " + id);
+        return produtoRepository.findById(id).orElse(null);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable("id") String id){
+        produtoRepository.deleteById(id);
+        System.out.println("Deletando produto com id: " + id);
+    }
+
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable("id") String id,
+                          @RequestBody Produto produto){
+        produto.setId(id);
+        produtoRepository.save(produto);
+        System.out.println("Atualizando produto com id: " + id);
 
     }
 }
